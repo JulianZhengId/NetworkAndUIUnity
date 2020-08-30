d10eb3ca2bc4dfce4013612cf35f1edac1bd1c90
JulianZhengId <zheng_julian@yahoo.com>
2020-08-30 12:48:36 +0200

    Add Network Code

---
+++ Assets/Network/CreateRoom.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateRoom : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private string _roomName;
+
+    public void ClickToCreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 3;
+        int roomNumber = Random.Range(1, 10000000);
+        _roomName = "Room " + roomNumber;
+        PhotonNetwork.JoinOrCreateRoom(_roomName, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Room has been created successfully with name " + _roomName);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Fail to Create Room");
+    }
+}
---
+++ Assets/Network/Networking.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Networking : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        print("Connecting To Server");
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Connected To Server as " + PhotonNetwork.LocalPlayer.NickName);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnect for reason " + cause.ToString());
+    }
+}
